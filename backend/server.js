1711acf8b7f60da95fd6717ccc91a1d7559833a6
Parsa <parsaj79@gmail.com>
2022-09-11 20:24:12 +0430

    path require in server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const path = require("path")
 
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
